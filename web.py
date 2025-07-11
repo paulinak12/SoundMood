b37f5b7c0117c1deb1d51d6e3706016e0ea98ca0
paulinak12 <a20221575@pucp.edu.pe>
2025-07-11 18:07:06 -0500

    Update web.py

--- web.py
+++ web.py
@@ -103,9 +103,11 @@ else:
         if proposito:
             # Filtro por el propósito "Que acompañe lo que siento"
             if proposito == 'Que acompañe lo que siento':
+                st.write("Filtrando por 'acompañar'...")  # Depuración
                 resultado = resultado[resultado['proposito'].str.lower() == 'acompañar']
             # Filtro por el propósito "Que mejore mi estado de ánimo"
             elif proposito == 'Que mejore mi estado de ánimo':
+                st.write("Filtrando por 'mejorar'...")  # Depuración
                 resultado = resultado[resultado['proposito'].str.lower() == 'mejorar']
 
         # Mostrar la canción recomendada si existe
@@ -132,3 +134,4 @@ else:
             st.write("No se encontraron canciones para tu selección.")
     else:
         st.write("Por favor selecciona todas las opciones para obtener una recomendación.")
+
