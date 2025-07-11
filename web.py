d0dfa2c73dd0f5caf9329457cc14b33ab5f224e2
paulinak12 <a20221575@pucp.edu.pe>
2025-07-11 17:12:29 -0500

    Update web.py

--- web.py
+++ web.py
@@ -18,14 +18,14 @@ if pagina_seleccionada == 'Presentación':
     texto = """
     ¡Hola! Somos Paulina Kosaka, Marcela Ismodes y Malena Aldazabal. Queremos darte la bienvenida a nuestra página. A continuación, te presentamos más información sobre el proyecto.
 
-    __🎧 SoundMood: Tu estado de ánimo tiene sonido¿Qué es SoundMood?__
+    \n__🎧 SoundMood: Tu estado de ánimo tiene sonido¿Qué es SoundMood?__
 
 SoundMood es una página web interactiva que busca conectar la música con las emociones de cada usuario. A través de una interfaz amigable y personalizada, ofrecemos recomendaciones de canciones basadas en el estado de ánimo actual de la persona. 
 
 Pero vamos más allá de una simple recomendación musical: personalizamos la experiencia según las preferencias del usuario en cuanto al idioma (español o inglés), la duración de la canción y el año de lanzamiento.
     Además, para enriquecer la experiencia musical, SoundMood también ofrece información detallada sobre el artista y la canción, permitiendo así que el usuario no solo escuche música, sino que también descubra y aprenda sobre lo que está escuchando.
     
-    __¿Por qué creamos SoundMood?__
+    \n__¿Por qué creamos SoundMood?__
     
 La música siempre ha sido una herramienta poderosa para conectar con las emociones humanas. Todos hemos buscado canciones cuando estamos tristes, queremos motivarnos o simplemente relajarnos. Sin embargo, no siempre sabemos qué escuchar o no encontramos algo que realmente encaje con cómo nos sentimos.
 
