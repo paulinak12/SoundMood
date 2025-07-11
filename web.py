abbab8c290ed3b849ee63e0011de9a14241d338a
paulinak12 <a20221575@pucp.edu.pe>
2025-07-11 17:10:14 -0500

    Update web.py

--- web.py
+++ web.py
@@ -30,6 +30,7 @@ Pero vamos más allá de una simple recomendación musical: personalizamos la ex
 La música siempre ha sido una herramienta poderosa para conectar con las emociones humanas. Todos hemos buscado canciones cuando estamos tristes, queremos motivarnos o simplemente relajarnos. Sin embargo, no siempre sabemos qué escuchar o no encontramos algo que realmente encaje con cómo nos sentimos.
 
 SoundMood nace para resolver ese problema, ofreciendo una plataforma sencilla pero efectiva que:
+
 Comprende lo que sientes
 
 Te recomienda música acorde a ese sentimiento
